e378ca2615e47afb74a4cd53742c636e8ab269c8
RukhshanCh <ch.rukhshan@gmail.com>
2026-05-10 03:25:55 +0500

    Add LightRays component for enhanced visual effects in Hero section

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,12 +1,27 @@
 import TypingEffect from "../components/TypingEffect"
+import LightRays from '../ReactBitComponents/LightRays'
 import CV from "../assets/CV.pdf"
 
 const Hero = () => {
     return (
         <section id="home" className="hero" style={{ position: 'relative', overflow: 'hidden', zIndex: 1 }}>
-            <canvas id="hero-canvas"></canvas>
+            <LightRays
+                raysOrigin="top-center"
+                raysColor="#ffffff"
+                raysSpeed={1}
+                lightSpread={0.5}
+                rayLength={3}
+                followMouse={true}
+                mouseInfluence={0.1}
+                noiseAmount={0}
+                distortion={0}
+                className="custom-rays"
+                pulsating={false}
+                fadeDistance={1}
+                saturation={1}
+            />
             <div className="hero-content">
-                <h1>Rukhshan <span style={{color: "#ff5e00"}}>Shahid</span></h1>
+                <h1>Rukhshan <span style={{ color: "#ff5e00" }}>Shahid</span></h1>
                 <div className="typed-text" id="typedElement">
                     <TypingEffect />
                 </div>
